60b397517dbbacd9f6e10c2b2b2212242755ea41
shuhaoliu <shuhaoliu@users.noreply.github.com>
2022-04-25 11:14:38 +0000

    jekyll build from Action ad89582c09692fb0e4f4a4a388444921e737f1b6

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Reading Notes: Atomic Habits",
+        "excerpt":"Reading notes of Atomic Habits: An Easy &amp; Proven Way to Build Good Habits &amp; Break from Bad Ones, by James Clear. This book introduces a framework for habit forming. Many approaches/tricks are highly practical and easy to follow. Core Ideas A Four-Step Model of Human Behavior The process of...","categories": ["reading"],
+        "tags": ["reading notes","habits"],
+        "url": "/pages/shuhaoliu/blog/reading/2021/reading-notes-atomic-habits/",
+        "teaser": "https://jamesclear.com/wp-content/uploads/2021/08/atomic-habits-dots-1.png"
+      }]
